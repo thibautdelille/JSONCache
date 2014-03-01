b2c61995b8cebf9f72b4fde6fa5b3690327bf704
Thibaut Delille <tdelille@gmail.com>
2014-03-01 15:54:47 -0800

    add the option page

--- jsoncache.php
+++ jsoncache.php
@@ -109,6 +109,77 @@ class JSONCache
 
 	/**
 	 *
+	 */
+	function add_options_page()
+	{
+		// Debug
+		$this->setDebug(true);
+
+		$b_post_content_is_saved = false;
+		$b_page_content_is_saved = false;
+
+		if(count($_POST) > 1 && isset($_POST['generate']) && $_POST['generate'] == 'oui'){
+      // Generate all content
+      if(isset($_POST['page']) && $_POST['page'] == '1')
+        if($this->generateContent('page') !== false)
+          $b_page_content_is_saved = true;
+
+      // Generate all content
+      if(isset($_POST['post']) && $_POST['post'] == '1')
+        if($this->generateContent('post') !== false)
+          $b_post_content_is_saved = true;
+
+		}
+
+		?>
+		<style type="text/css">
+		.important_notice { width:900px;padding:10px;font-size:14px;color:#000;border:5px #e52020 solid;height:100px;margin-top:10px; }
+		.important_notice p { line-height:20px;float:left;width:840px; }
+		.important_notice img { float:left;margin-right:10px;margin-top:15px; }
+		.result img, .result h3 { float:left; }
+		.result h3 { margin-top:6px;margin-left:12px;font-size:16px; }
+		p.submit span { font-size:14px; }
+		.results_message { clear:both;width:400px; }
+		</style>
+		<div class="wrap">
+			<h2>JSON CACHE SYSTEM</h2>
+			<div class="important_notice">
+				<img src="../wp-content/plugins/JSONcache/warning.png" />
+				<p>
+				This plugin handles the cached content on the site.<br/>
+				When you click on the button <b><i>Generate Cached Content</i></b>, all the site content will be generated in a server-cache format (JSON).<br/>
+				This improves the general loading of the site.<br/>
+				</p>
+			</div>
+			<form action="" method="post">
+				<input type="hidden" name="generate" value="oui"/>
+				<p class="submit">
+					<input type="checkbox" name="page" value="1" checked="checked"/> <span>Generate page content</span><br/><br/>
+          <input type="checkbox" name="post" value="1" checked="checked"/> <span>Generate post content</span><br/><br/>
+					<input type="submit" name="submit" style="font-size:16px !important;" id="submit" class="button-primary" value="Generate Cached Content" />
+				</p>
+			</form>
+			<div class="result">
+				<?php if($b_post_content_is_saved){?>
+				<div class="results_message">
+					<img src="../wp-content/plugins/JSONcache/check-icon.png"/>
+					<h3>Page cache generated</h3>
+				</div>
+				<?php }?>
+				<?php if($b_page_content_is_saved){?>
+				<div class="results_message">
+					<img src="../wp-content/plugins/JSONcache/check-icon.png"/>
+					<h3>Post cache generated</h3>
+				</div>
+				<?php }?>
+			</div>
+		</div>
+		<?php
+	}
+
+
+	/**
+	 *
 	 * @param unknown_type $p_debug
 	 */
 	function setDebug($p_debug)
